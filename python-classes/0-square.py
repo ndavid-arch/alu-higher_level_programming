1b23419ba347a3b07abf6b13b341cc71ee564f60
ndavid-arch <n.david@alustudent.com>
2025-11-14 17:06:34 +0200

    square class and test files

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Create an Empty Square """
+
+
+class Square:
+    """Create a square class object"""
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Create a class Square"""
+
+
+class Square:
+    """ Create a class object """
+    def __init__(self, size):
+        self.__size = size
+    """ initializes a square object with parameters
+    args:
+        __size(int): size of the square """
