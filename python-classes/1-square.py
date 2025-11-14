26693b45493ea22921f0a9b2efb145853ad8db93
ndavid-arch <n.david@alustudent.com>
2025-11-14 18:14:45 +0200

    squares

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,7 +6,3 @@ class Square:
     """ Create a class object """
     def __init__(self, size):
         self.__size = size
-    """ initializes a square object with parameters
-    args:
-        __size(int): size of the square """
-
