384409bb8e30dbb11b1c1fff7ad13cd6898eb679
ndavid-arch <n.david@alustudent.com>
2025-11-27 14:02:17 +0000

    remove PEP8 whitespace errors in 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Define a function that reads text files."""
-
-
 def read_file(filename=""):
     """A function to read a text file."""
     with open(filename, 'r', encoding='UTF8') as b:
