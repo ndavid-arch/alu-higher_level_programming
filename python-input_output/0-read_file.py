5062a8872c0205f62d5596dd05e7810529f44e62
ndavid-arch <n.david@alustudent.com>
2025-11-27 14:03:37 +0000

    expected blank lines

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Define a function that reads text files."""
+
+
 def read_file(filename=""):
     """A function to read a text file."""
     with open(filename, 'r', encoding='UTF8') as b:
