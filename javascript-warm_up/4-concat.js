0c21d5ab6d1fd1d74b3c23cdf780e7f8c88b4451
NTWALI BENI DAVID <n.david@alustudent.com>
2026-01-31 01:34:56 +0000

    argument plus message

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 
 if (args.length === 2) {
-	console.log(args[0] + ' is ' + args[1] );
+	console.log(args[0] + ' is ' + args[1]);
 } else if (args.length === 1) { 
 	console.log(args[0] + ' is undefined');
 } else {
